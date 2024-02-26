c15fc0fd34e5af1921fa57f1b6e15e8ac88ea4b5
karinEkenberg <zirkon6@hotmail.com>
2024-02-26 09:19:21 +0100

    Add project files.

---
+++ Other.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LibExcercise
+{
+    public class Other
+    {
+
+        public static void Warning(string text) 
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("---------------------------");
+            WriteLine($"{text}");
+            WriteLine("---------------------------");
+            ForegroundColor = ConsoleColor.Magenta;
+            Thread.Sleep(2000);
+        }
+
+        public static void Text(string text)
+        {
+            Clear();
+            ForegroundColor = ConsoleColor.Green;
+            WriteLine("---------------------------");
+            WriteLine($"{text}");
+            WriteLine("---------------------------");
+            ForegroundColor = ConsoleColor.Magenta;
+            Thread.Sleep(1000);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using static System.Console;
+
+namespace LibExcercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+
+            while (run)
+            {
+                Other other = new Other();
+                Book book = new Book();
+                Lender lender = new Lender();
+                Library library = new Library();
+
+                Clear();
+                ForegroundColor = ConsoleColor.Magenta;
+                WriteLine("--------------------------------------");
+                WriteLine("[1] - Add books.");
+                WriteLine("[2] - Lend book/s.");
+                WriteLine("[3] - Return book/s.");
+                WriteLine("[4] - Show available books.");
+                WriteLine("[5] - Show lenders and what they lent.");
+                WriteLine("[6] - Exit program.");
+                WriteLine("--------------------------------------");
+
+                Int32.TryParse(ReadLine(), out int input);
+
+                switch (input)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        Other.Text("Shutting down...");
+                        run = false;
+                        return;
+                    default:
+                        Other.Warning("Wrong input.. Try 1-6...");
+                        break;
+                }
+            }
+        }
+    }
+}
